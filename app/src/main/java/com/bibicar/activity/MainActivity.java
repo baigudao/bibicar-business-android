9370479937257300fe4306630a04bb67a2fe7aaa
Jackie <971060378@qq.com>
2017-06-13 17:54:59 +0800

    optimization

--- app/src/main/java/com/bibicar/activity/MainActivity.java
+++ app/src/main/java/com/bibicar/activity/MainActivity.java
@@ -2,7 +2,6 @@ package com.bibicar.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
 
 import com.bibicar.R;
 
@@ -11,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
-        setContentView(R.layout.fragment_home);//首页页面
+        //                setContentView(R.layout.fragment_home);//首页页面
+        setContentView(R.layout.fragment_content_customization);//内容定制页面
     }
 }
